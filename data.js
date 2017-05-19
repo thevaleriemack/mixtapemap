02b9fdbce4771a0ba93728e8e63d413365c741f3
Val <dgm97@cornell.edu>
2017-05-19 02:13:24 -0400

    need to fix artistsong and similarsongs

--- data.js
+++ data.js
@@ -1,1 +1,6 @@
-
+// The following getRandomIntIncluseive function was found on this webpage: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
