ae00b220a8e13275a6ed63cfa3ad084537cf461b
Val <dgm97@cornell.edu>
2017-05-19 11:34:51 -0400

    completed styling, started error handling, modified ajax

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 README.md
-
-reference: https://dribbble.com/shots/2707834-VIA-Mixtape-Flow/attachments/547472
--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+function reportError(err) {
+    $('#errorMsg').html(err);
+}
+
 // The following getRandomIntIncluseive function was found on this webpage: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+$(document).on('click', '#about', function () {
+    $('#splash').css('display', 'block');
+});
+
+$(document).on('click', '#closeSplash', function () {
+    $('#splash').css('display', 'none');
+});
